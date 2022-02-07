faa1fbdbaf7fd5ec5925be812f5910775a852021
Randika Abeyrathna <randika.l.abeyrathna@gmail.com>
2022-02-07 14:48:24 +0530

    Initial commit

---
+++ src/main/java/com/tsd/armsystem/AutomatedResourcesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tsd.armsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedResourcesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedResourcesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsd/armsystem/AutomatedResourcesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsd.armsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedResourcesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
