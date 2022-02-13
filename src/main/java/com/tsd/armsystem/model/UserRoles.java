647f3990952260a9d43b10aa12d63526515c8475
Randika Abeyrathna <randika.l.abeyrathna@gmail.com>
2022-02-13 12:19:28 +0530

    Created model classes and added Data JPA for pom

---
+++ src/main/java/com/tsd/armsystem/model/UserRoles.java
@@ -0,0 +1,26 @@
+package com.tsd.armsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRoles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_nic",referencedColumnName = "nic")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id",referencedColumnName = "id")
+    private Role role;
+}
