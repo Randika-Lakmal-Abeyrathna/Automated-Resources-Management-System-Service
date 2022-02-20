be6d379734e25639056970bcebb9ce9a45fca148
tharangarandunuveera <randunuveera.1995@gmail.com>
2022-02-20 22:36:50 +0530

    Test Commit

---
+++ src/main/java/com/tsd/armsystem/service/TestService.java
@@ -0,0 +1,12 @@
+package com.tsd.armsystem.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class TestService {
+}
