eaf4e9f36bc2db0ef5d407342b38e659e59b291f
Manura-Deelaka <manura.deelaka@gmail.com>
2022-03-07 20:10:42 +0530

    sent email function

---
+++ src/main/java/com/tsd/armsystem/exception/MailException.java
@@ -0,0 +1,9 @@
+package com.tsd.armsystem.exception;
+
+public class MailException extends RuntimeException {
+
+    public MailException(String message){
+
+        super(message);
+    }
+}
---
+++ src/main/java/com/tsd/armsystem/service/MailService.java
@@ -0,0 +1,41 @@
+package com.tsd.armsystem.service;
+
+import com.tsd.armsystem.exception.MailException;
+import com.tsd.armsystem.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+    private final JavaMailSender javaMailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+
+    @Async
+    public void sendMail(NotificationEmail notificationEmail){
+
+        MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom("resroucemanagementtsd@gmail.com");
+            mimeMessageHelper.setTo(notificationEmail.getRecipient());
+            mimeMessageHelper.setSubject(notificationEmail.getSubject());
+            mimeMessageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+
+        try{
+            javaMailSender.send(mimeMessagePreparator);
+            log.info("Activation email sent!");
+        }catch (MailException e){
+            e.printStackTrace();
+            // throw new EmailException("Exception occurred when sending email to "+notificationEmail.getRecipient());
+        }
+
+    }
+}
