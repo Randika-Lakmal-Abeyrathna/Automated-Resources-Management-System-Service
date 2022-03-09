cca0ddc6459d365303796c078397a4f5f421c5e5
Randika Abeyrathna <randika.l.abeyrathna@gmail.com>
2022-03-09 15:45:00 +0530

    Former Experience model added

---
+++ src/main/java/com/tsd/armsystem/model/FormerExperiance.java
@@ -0,0 +1,29 @@
+package com.tsd.armsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class FormerExperiance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idformerexperiance;
+    private Date appointntdate;
+    private Date appointmentenddate;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "school_idschool",referencedColumnName = "idschool")
+    private School school;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "teacher_id",referencedColumnName = "id")
+    private Teacher teacher;
+}
