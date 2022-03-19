eb7d8fa44b133959add625b2d345c90b5aa56fd9
Hasmie Wijerathna <hasmieneema@gmail.com>
2022-03-19 15:30:24 +0530

    save teacher subject

---
+++ src/main/java/com/tsd/armsystem/dto/TeacherSubjectRequest.java
@@ -0,0 +1,14 @@
+package com.tsd.armsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class TeacherSubjectRequest {
+
+    private int teacherId;
+    private int subjectId;
+}
