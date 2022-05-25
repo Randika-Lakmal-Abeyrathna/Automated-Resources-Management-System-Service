0afa65af7564a99eef222ddcef6949ddbe2ef4d8
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2022-05-26 01:20:06 +0530

    Added Logging to the project with rolling logging path =/logs

---
+++ src/main/java/com/tsd/armsystem/logger/LoggingAdvisor.java
@@ -0,0 +1,62 @@
+package com.tsd.armsystem.logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAdvisor {
+
+    Logger log= LoggerFactory.getLogger(LoggingAdvisor.class);
+
+    @Pointcut(value = "execution(* com.tsd.armsystem.*.*.*(..))")
+    public void projectPointCut(){
+
+    }
+
+    @Pointcut(value = "execution(* com.tsd.armsystem.service.*.*(..))")
+    public void servicePointCut(){
+
+    }
+
+    @Pointcut(value = "execution(* com.tsd.armsystem.controller.*.*(..))")
+    public void controllerPointCut(){
+
+    }
+
+    @Pointcut(value = "execution(* com.tsd.armsystem.repository.*.*(..))")
+    public void repositoryPointCut(){
+
+    }
+
+
+    //    @Around("servicePointCut() && controllerPointCut()")
+    @Around("projectPointCut()")
+    public Object applicationLogger(ProceedingJoinPoint pjp) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String methodName = pjp.getSignature().getName();
+        String className = pjp.getTarget().getClass().toString();
+        Object[] array = pjp.getArgs();
+
+        Object proceed=null;
+        try {
+            log.info("Method invoked " + className + " : " + methodName + "() " + "arguments : " + mapper.writeValueAsString(array));
+
+            proceed = pjp.proceed();
+            log.info(className+ " : "+methodName+"() "+"Response : "+mapper.writeValueAsString(proceed));
+        }catch (Throwable e){
+
+        }
+
+
+
+        return proceed;
+    }
+}
