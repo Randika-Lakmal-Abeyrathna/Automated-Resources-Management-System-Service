abedcad65308f00c4847b37a12bf0b2e640295eb
Randika Lakmal <randila.l.abeyrathna@gmail.com>
2022-11-26 02:37:43 +0530

    Fix the issue with not loading admin province Updated the repository to native Query

--- src/main/java/com/tsd/armsystem/repository/AdminProvinceRepository.java
+++ src/main/java/com/tsd/armsystem/repository/AdminProvinceRepository.java
@@ -3,6 +3,7 @@ package com.tsd.armsystem.repository;
 import com.tsd.armsystem.model.AdminProvince;
 import com.tsd.armsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface AdminProvinceRepository extends JpaRepository<AdminProvince,Integer> {
 
-    List<AdminProvince> findByUser(User user);
+    @Query(value = "SELECT * FROM admin_province a where user_nic= ?1",nativeQuery = true)
+    List<AdminProvince> findByUser(String nic);
 
 }
