c451da1a62b1efafbb7f0a2116af4ac5a56d1c21
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2023-08-01 06:08:02 +0530

    Removed unused imports

--- src/main/java/com/tsd/armsystem/service/AdminProvinceService.java
+++ src/main/java/com/tsd/armsystem/service/AdminProvinceService.java
@@ -1,7 +1,6 @@
 package com.tsd.armsystem.service;
 
 import com.tsd.armsystem.model.AdminProvince;
-import com.tsd.armsystem.model.User;
 import com.tsd.armsystem.repository.AdminProvinceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +14,6 @@ import java.util.List;
 public class AdminProvinceService {
 
     private final AdminProvinceRepository adminProvinceRepository;
-    private final UserService userService;
 
     public List<AdminProvince> getAdminProvinceByUser(String nic){
         return adminProvinceRepository.findByUser(nic);
